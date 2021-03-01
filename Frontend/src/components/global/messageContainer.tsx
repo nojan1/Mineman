12d56691bdf66cb6916497914448e0688c0271fd
Niklas Hedlund <niklas.hedlund@tretton37.com>
2021-03-01 09:48:41 +0100

    Switched from Alert to Toast in message container

--- Frontend/src/components/global/messageContainer.tsx
+++ Frontend/src/components/global/messageContainer.tsx
@@ -1,26 +1,54 @@
 import styled from '@emotion/styled';
 import React from 'react';
-import { Alert } from 'react-bootstrap';
+import { Toast } from 'react-bootstrap';
 import { clearMessage } from '../../actions/messages';
+import { Message } from '../../models/message';
 import { getState } from '../../state';
 
 const OuterMessageContainer = styled.div`
     position: absolute;
-    bottom: 0;
-    left: 5px;
-    max-width: 500px;
+    bottom: 10px;
+    left: 10px;
+    width: 100%;
+    max-width: 350px;
 `;
 
-const MessageContainer:React.FunctionComponent = () => {
-    const { state: {messages}, dispatch} = getState();
+
+const getStyle = (message: Message): any => {
+    const baseStyle= {
+        padding: '5px',
+    }
+
+    if (message.type == 'danger') {
+        return {
+            ...baseStyle,
+            backgroundColor: '#d4441a',
+            color: 'white'
+        }
+    } else {
+        return { baseStyle };
+    }
+}
+
+const MessageContainer: React.FunctionComponent = () => {
+    const { state: { messages }, dispatch } = getState();
 
     return (
         <OuterMessageContainer>
-            {messages.map((message,i) => 
-                <Alert variant={message.type} key={i} dismissible onClose={() => clearMessage(dispatch, message)}>
-                    <Alert.Heading>{message.title}</Alert.Heading>
-                    {message.content ? <p>{message.content}</p> : null}
-                </Alert>
+            {messages.map((message, i) =>
+                <Toast
+                    key={i}
+                    onClose={() => clearMessage(dispatch, message)}
+                    show={true}
+                    delay={5000}
+                    // autohide
+                    style={getStyle(message)}
+                >
+                    <Toast.Header>
+                        <strong className="mr-auto">{message.title}</strong>
+                    </Toast.Header>
+                    <Toast.Body>{message.content}</Toast.Body>
+                </Toast>
             )}
         </OuterMessageContainer>
     );
